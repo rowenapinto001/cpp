00db81c6ac8667432590ffa5b61832ea844f1db1
rowenapinto001 <rowenapinto001@gmail.com>
2023-11-01 18:52:26 +0530

    string

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+   using namespace std;
+
+   int main() {
+      string s = "Striver";
+      int len = s.size();
+      s[len-1] = 'z';
+      cout << s[len - 1];
+
+   }
+
+
+
