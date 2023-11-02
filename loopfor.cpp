1ae4ce110eb78c4ea9ec5f4b13ab5a2697ae4a69
rowenapinto001 <rowenapinto001@gmail.com>
2023-11-02 20:38:01 +0530

    loop for

---
+++ loopfor.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+   using namespace std;
+
+   int main() {
+      // i = 1
+      // i = 2
+      // i = 3
+      // i = 4
+      // i = 5
+      // i = 6
+      for(int i = 1;i = i + 1) {
+         cout << "Striver" << endl;
+      }
+      cout << i << endl;
+
+
+
+   }
+
