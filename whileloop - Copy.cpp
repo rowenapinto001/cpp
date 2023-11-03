d94aa1865ec18a356cbe2f5556d7e98a9d4b4797
rowenapinto001 <rowenapinto001@gmail.com>
2023-11-03 20:51:14 +0530

    whileloop

---
+++ whileloop - Copy.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+   using namespace std;
+
+   int main() {
+      // i = 1
+      // i = 2
+      // i = 3
+      // i = 4
+      // i = 5
+      // i = 6
+      // int i;
+      //for(int i = 1;i <= 25; i = i + 5) {
+      //    cout << "Striver" << endl;
+      //}
+      //cout << i << endl;
+
+
+      int i = 2;
+      do {
+         cout << "Striver" << i << endl;
+         i = i + 1;
+      }while(i<=1);
+         cout << i << endl;
+         
+      }
+
+
