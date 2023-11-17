bbcc4a7426e7489f6f92ad67fb0a3af963c4d287
rowenapinto001 <rowenapinto001@gmail.com>
2023-11-17 19:22:10 +0530

    functions

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// functions are set of code which performs something for you
+// functions are used to modularise code
+// functions are used to increase readability
+// functions are ysed to use same code multiple times
+// void -> which does not returns anything
+// reurn
+// parameterised
+// non parameterised
+
+void printname(string name) {
+	cout << "hey" << name;
+}
+int main() {
+	string name;
+	cin >> name;
+	printname(name);
+} 
